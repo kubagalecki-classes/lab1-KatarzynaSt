0bac279affe3b16851fc8757bcdc2293c9419f26
KatarzynaSt <katarzyna.steczniewska.stud@pw.edu.pl>
2020-11-02 22:20:00 +0000

    wersja1

--- zal/Wektor2D.hpp
+++ zal/Wektor2D.hpp
@@ -1,35 +1,28 @@
 #include <iostream>
 #include <math.h>
 
-struct Wektor2D
-{ 
-    Wektor2D (double x, double y){};
-    //double xx   = pow(x, 2);
-    //double yy   = pow(y, 2);
-    //double sum  = xx + yy;
-    //double norm = pow(sum, 0.5);
-    //void   printWspol()
-    //{
-      //  std::cout << "wspolrzedna wektora x = " << x << ",\n wspolrzedna wektora y = " << y << "\n";
-    //}
-};
-
-int main()
+class Wektor2D 
 {
-    Wektor2D v1;
-    v1.printWspol();
-    std::cout << "Norma tego wektora = " << v1.norm << ".\n";
-    std::cout << "wspolrzedna wektora x = " << v1.x << ",\n wspolrzedna wektora y = " << v1.y << "\n";
+  private:
+  		double x;
+  		double y;
+	  	
+	public:
+  		
+  		//Konstruktor domyślny, wektor o wsp. [0, 0]
+
+      v1()
+      {
+        x=0; y=0;
+      }
+// Konstruktor nadający współrzędne
+v2(double xx, double yy=2); 
+
+
 }
 
-//Wektor2D v1{};           // Konstruktor domyślny, wektor o wsp. [0, 0]
-//v1.setX(1.);             // setter
-//v1.setY(1.);             // setter
-//double x1 = v1.getX(1.); // getter
-//double y1 = v1.getY(1.); // getter
- 
-//Wektor2D v2{2., 2.}; // Konstruktor nadający współrzędne
- 
-//Wektor2D sum = v1 + v2; // dodawanie wektorów
- 
-//double prod = v1 * v2; // iloczyn skalarny
+Wektor2D::v2 (double xx, double yy)
+{
+  x=xx;
+  y=xx;
+}
