5451ab172c39b9ee51f0ce0b0d469d29dc136c43
KatarzynaSt <katarzyna.steczniewska.stud@pw.edu.pl>
2020-11-02 21:58:37 +0000

    lab1 tylko

--- lab1.cpp
+++ lab1.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <math.h>
+
+struct Wektor2D
+{
+    double x;
+    double y;
+    double xx   = pow(x, 2);
+    double yy   = pow(y, 2);
+    double sum  = xx + yy;
+    double norm = pow(sum, 0.5);
+    void   printWspol()
+    {
+        std::cout << "wspolrzedna wektora x = " << x << ",\n wspolrzedna wektora y = " << y << "\n";
+    }
+};
 
 int main()
 {
-    puts("Hello, World!");
+    Wektor2D wek;
+    wek.x = 2;
+    wek.y = -3;
+    wek.printWspol();
+    std::cout << "Norma tego wektora = " << wek.norm << ".\n";
 }
